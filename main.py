a8a1190cfee3dcb0a3369890a98af2074e426332
moaaz814 <muhammad.moaaz@gaditek.com>
2025-01-22 18:27:45 +0500

    Fixed small errors

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ from langchain_core.output_parsers import StrOutputParser
 def generate_system_message(preferences: Dict[str, str]):
     base_msg = """You are a knowledgeable and friendly chef and health assistant. Your role is to strictly answer questions and provide information ONLY about cuisines, recipes, cooking techniques, and general health advice related to food and nutrition, based on the user's preferences.
     If a user asks about a topic that is unrelated to cooking or health, politely inform them that you can only assist with food and health-related queries. Never make assumptions—always ask the user for clarification if their query is unclear or you need more details to provide an accurate response.
-    If the query contains any image url, or if it contains the user asking a question about any image, then you MUST respond to the answer properly, without any errors."""
+    If the query contains any image url, or if it contains the user asking a question about any image, then you MUST respond to the answer properly, without any errors, and without responding with a pre-defined response, you should explain that image."""
 
     prefs_msg = " ".join([f"{key}: {value}." for key, value in preferences.items()])
     return f"{base_msg} User preferences: {prefs_msg}"
@@ -52,7 +52,7 @@ def call_model(state: MessagesState, config: RunnableConfig):
     system_msg = generate_system_message(preferences)
 
     print(state['messages'][-1].content[0])
-
+ 
     response = model.invoke(
         [{"role": "user", "content": system_msg}] + state['messages']
     )
